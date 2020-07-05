e6630b63036b019b2dd7c0b67ddd09b6bbbaa74c
Saswata Mukherjee <saswataminsta@yahoo.com>
2020-07-05 20:13:31 +0530

    Add TabView

--- FurnitureStore/ContentView.swift
+++ FurnitureStore/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
 
 struct TabView: View {
     @State var index = 0
-    
+
     var body: some View {
         VStack(spacing: 0) {
             Spacer()
@@ -39,9 +39,92 @@ struct TabView: View {
                     .clipShape(Capsule())
                     
                     Spacer(minLength: 0)
+                    
+                    Button(action: {
+                        self.index = 1
+                    }) {
+                        HStack(spacing: 6) {
+                            Image("search")
+                                .foregroundColor(self.index == 1 ? .white : .primary)
+                            
+                            if self.index == 1{
+                                
+                                Text("Search")
+                                    .foregroundColor(.white)
+                            }
+                        }.padding(.vertical,10)
+                        .padding(.horizontal)
+                        .background(self.index == 1 ? Color("Color") : Color.clear)
+                        .clipShape(Capsule())
+                        
+                        
+                        Spacer(minLength: 0)
+                        
+                        Button(action: {
+                            
+                            self.index = 2
+                            
+                        }) {
+                            
+                            HStack(spacing: 6){
+                             
+                                Image("cart")
+                                    // dark mode adoption...
+                                    .foregroundColor(self.index == 2 ? .white : .primary)
+                                
+                                if self.index == 2{
+                                    
+                                    Text("Cart")
+                                        .foregroundColor(.white)
+                                }
+                                
+                            }
+                            .padding(.vertical,10)
+                            .padding(.horizontal)
+                            .background(self.index == 2 ? Color("Color") : Color.clear)
+                            .clipShape(Capsule())
+                        }
+                        
+                        Spacer(minLength: 0)
+                        
+                        Button(action: {
+                            
+                            self.index = 3
+                            
+                        }) {
+                            
+                            HStack(spacing: 6){
+                             
+                                Image("account")
+                                    // dark mode adoption...
+                                    .foregroundColor(self.index == 3 ? .white : .primary)
+                                
+                                if self.index == 3{
+                                    
+                                    Text("Account")
+                                        .foregroundColor(.white)
+                                }
+                                
+                            }
+                            .padding(.vertical,10)
+                            .padding(.horizontal)
+                            .background(self.index == 3 ? Color("Color") : Color.clear)
+                            .clipShape(Capsule())
+                        }
 
+                        
+                        
+                    }
+                    .padding(.horizontal,25)
+                    .padding(.top)
+                    // based on device bottom padding will be changed...
+                    .padding(.bottom,UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 10 : UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+                    // for shadow...
+                    .shadow(color: Color.primary.opacity(0.08), radius: 5, x: 5, y: -5)
 
                 }
+                .edgesIgnoringSafeArea(.bottom)
+
             }
         }
     }
